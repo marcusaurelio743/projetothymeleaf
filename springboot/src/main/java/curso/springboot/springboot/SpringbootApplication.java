2b9d9136cc6ac212d62b1ab8150b8a14a98a072b
marcus aurelio <marcusaurelio743@gmail.com>
2023-08-20 18:05:51 -0300

    adicionando alguns recursos do thymeleaf e listando usuarios cadastrados do banco

--- springboot/src/main/java/curso/springboot/springboot/SpringbootApplication.java
+++ springboot/src/main/java/curso/springboot/springboot/SpringbootApplication.java
@@ -3,6 +3,7 @@ package curso.springboot.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringbootApplication {
 
--- springboot/src/main/java/curso/springboot/springboot/repository/PessoaRepository.java
+++ springboot/src/main/java/curso/springboot/springboot/repository/PessoaRepository.java
@@ -1,13 +1,10 @@
 package curso.springboot.springboot.repository;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import curso.springboot.springboot.model.Pessoa;
 
-@Repository
-@Transactional
+
 public interface PessoaRepository extends CrudRepository<Pessoa, Long>{
 
 }
